918046689a24070a1619f1ccd5a9e647aeb329e8
BharathArya <bkvs007@gmail.com>
2017-09-25 20:47:44 +0530

    UI Changes

---
+++ app/src/main/java/com/example/bharath/silence_v10/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.example.bharath.silence_v10;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Bharath on 25-09-2017.
+ */
+
+public class SettingsFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.settings, container, false);
+        return myView;
+    }
+
+}
