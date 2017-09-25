0a30b3941eb4a26fdf749c67787e276bec2a57f2
BharathArya <bkvs007@gmail.com>
2017-09-25 07:37:19 +0530

    Initial Commit

--- app/src/main/java/com/example/bharath/silence_v10/MainActivity.java
+++ app/src/main/java/com/example/bharath/silence_v10/MainActivity.java
@@ -23,19 +23,9 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -80,13 +70,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.dashboard) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.namaz) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.personalization) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.settings) {
+
+        } else if (id == R.id.about) {
 
         } else if (id == R.id.nav_share) {
 
